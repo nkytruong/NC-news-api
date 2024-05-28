d84a9bed7e8101aa51e09f1e0e14c8a16b6bf61e
Nikki Truong <nikki@Nikkis-MacBook-Pro.local>
2024-05-28 13:30:07 +0100

    Adds a GET request for all endpoints

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express")
 const app = express()
 const {getTopics} = require("./db/controllers/topics-controllers")
+const {getEndpoints} = require("./db/controllers/app-controllers")
 
 app.get("/api/topics", getTopics)
+app.get("/api", getEndpoints)
+
+
 
 module.exports = app
